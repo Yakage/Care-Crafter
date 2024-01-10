690f4b0ffc5e7920e76f57d379df62f08d045103
Mark Benedict Abalos <benedictabalos30@gmail.com>
2024-01-10 14:36:36 +0800

    Create the splashscreen, intro sliders and login and signup.

---
+++ app/src/main/java/com/carecrafter/introduction/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.carecrafter.introduction
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.carecrafter.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
---
+++ app/src/main/java/com/carecrafter/registration/SignIn.kt
@@ -0,0 +1,12 @@
+package com.carecrafter.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.carecrafter.R
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
