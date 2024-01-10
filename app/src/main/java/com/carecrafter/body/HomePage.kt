8ee644f2ecbe70bbcfeb19a613de9048916afea5
Mark Benedict Abalos <benedictabalos30@gmail.com>
2024-01-10 15:49:45 +0800

    add the home page

---
+++ app/src/main/java/com/carecrafter/body/HomePage.kt
@@ -0,0 +1,12 @@
+package com.carecrafter.body
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.carecrafter.R
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
