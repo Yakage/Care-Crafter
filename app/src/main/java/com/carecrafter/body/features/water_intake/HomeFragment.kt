0f71159518bc9fba292c3b59c37bba6ca3811cc0
Mark Benedict Abalos <benedictabalos30@gmail.com>
2024-02-06 13:44:28 +0800

    Add circle progress bar for water intake

--- app/src/main/java/com/carecrafter/body/features/water_intake/HomeFragment.kt
+++ app/src/main/java/com/carecrafter/body/features/water_intake/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.carecrafter.body.features.water_intake
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,14 +12,30 @@ import com.carecrafter.databinding.WaterIntakeHomeBinding
 
 class HomeFragment : Fragment() {
     private lateinit var binding: WaterIntakeHomeBinding
+    private var count = 0f
+    private var total = 100f
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = WaterIntakeHomeBinding.inflate(inflater, container, false)
-
-
+        binding.tvCount.text = count.toString()
+        binding.tvTotal.text = total.toString() + " oz"
+        binding.circularProgressBar.apply {
+            progressMax = total
+            setProgressWithAnimation(00f, 1000)
+            progressBarWidth = 5f
+            backgroundProgressBarWidth = 7f
+            progressBarColor = Color.WHITE
+        }
+        binding.layoutCircleProgressBar.setOnClickListener {
+            count = count + 10f
+            binding.circularProgressBar.apply {
+                setProgressWithAnimation(count, 1000)
+            }
+            binding.tvCount.text = count.toString()
+        }
         return binding.root
     }
 
