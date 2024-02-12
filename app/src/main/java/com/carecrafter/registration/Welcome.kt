5fe6f416cb004bbc6d48df703f9689fa56681b66
Dalrymple Ramos <d4rukun@gmail.com>
2024-02-12 17:29:04 +0800

    splashscreen-regis/login system revamp

---
+++ app/src/main/java/com/carecrafter/registration/Welcome.kt
@@ -0,0 +1,31 @@
+package com.carecrafter.registration
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.carecrafter.R
+
+class Welcome : AppCompatActivity() {
+
+    private lateinit var welcomeLogin: Button
+    private lateinit var welcomeSignup: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+
+        welcomeLogin = findViewById(R.id.welcomeLogin)
+        welcomeSignup = findViewById(R.id.welcomeSignup)
+
+        welcomeLogin.setOnClickListener {
+            val intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
+
+        welcomeSignup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+    }
+}
