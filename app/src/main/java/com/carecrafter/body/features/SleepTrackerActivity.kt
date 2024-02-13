1e84f4d3444fe536358b136e84549197f4df678c
Raven <jerwincah4712@gmail.com>
2024-02-13 14:58:04 +0800

    Added onclicklisner to Sleep Tracker kt files

--- app/src/main/java/com/carecrafter/body/features/SleepTrackerActivity.kt
+++ app/src/main/java/com/carecrafter/body/features/SleepTrackerActivity.kt
@@ -7,6 +7,6 @@ import com.carecrafter.R
 class SleepTrackerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sleep_reminder)
+        setContentView(R.layout.activity_sleep_tracker)
     }
 }
--- app/src/main/java/com/carecrafter/body/features/sleep_tracker/HomeSleepTrackerFragment.kt
+++ app/src/main/java/com/carecrafter/body/features/sleep_tracker/HomeSleepTrackerFragment.kt
@@ -5,56 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 import com.carecrafter.R
+import com.carecrafter.body.features.step_tracker.HomeStepTrackerFragment
+import com.carecrafter.databinding.SleepTrackerHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeSleepTrackerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeSleepTrackerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: SleepTrackerHomeBinding
+    private lateinit var ivAlarm: ImageView
+    private lateinit var btStop: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.sleep_tracker_home, container, false)
-    }
+    ): View {
+        binding = SleepTrackerHomeBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeSleepTrackerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeSleepTrackerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding.ivAlarm.setOnClickListener{
+            findNavController().navigate(R.id.action_homeSleepTrackerFragment_to_settingsSleepTrackerFragment)
+        }
+        binding.btStop.setOnClickListener{
+            findNavController().navigate(R.id.action_homeSleepTrackerFragment_to_statisticSleepTrackerFragment)
+        }
+        return binding.root
     }
 }
